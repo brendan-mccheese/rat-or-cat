538cbb93b54938a66dbccaaf8dc9e38faff88102
Brendan Cheshire <brendancheshire@gmail.com>
2020-01-28 23:30:38 +0000

    Improved tests

--- packages/frontend/src/app/modules/main-game/question/question.component.spec.ts
+++ packages/frontend/src/app/modules/main-game/question/question.component.spec.ts
@@ -8,10 +8,13 @@ import { MockComponent } from "ng-mocks";
 import { CardComponent } from "../../../core/card/card.component";
 import { provideMockStore } from "@ngrx/store/testing";
 import { of } from "rxjs";
+import { answerQuestion } from "../../../store/actions/answer.actions";
 
 describe("QuestionComponent", () => {
     let component: QuestionComponent;
     let fixture: ComponentFixture<QuestionComponent>;
+    let mockStore: Store<any>;
+    let dispatchSpy: jest.SpyInstance;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -30,7 +33,9 @@ describe("QuestionComponent", () => {
                 provideMockStore({}),
                 {
                     provide: Router,
-                    useValue: {},
+                    useValue: {
+                        navigateByUrl: jest.fn(),
+                    },
                 },
                 {
                     provide: HttpClient,
@@ -44,11 +49,36 @@ describe("QuestionComponent", () => {
 
     beforeEach(() => {
         fixture = TestBed.createComponent(QuestionComponent);
+        mockStore = TestBed.get(Store);
+        dispatchSpy = jest.spyOn(mockStore, "dispatch");
         component = fixture.componentInstance;
         fixture.detectChanges();
+        jest.clearAllMocks();
     });
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    describe("answering question", () => {
+      let navigateMock: jest.SpyInstance;
+      beforeEach(() => {
+        navigateMock = (TestBed.get(Router) as Router).navigateByUrl as jest.Mock;
+        component.answerQuestion("CAT", "RAT");
+      });
+
+      test("dispatches action", () => {
+        expect(dispatchSpy).toHaveBeenCalledWith(
+          answerQuestion({
+            answer: "CAT",
+            correctAnswer: "RAT",
+            questionNo: 1,
+          }),
+        );
+      });
+
+      test("navigates to next question", () => {
+        expect(navigateMock).toHaveBeenCalledWith("/game/question-2");
+      });
     });
 });
